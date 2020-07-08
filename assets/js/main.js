b2ca488bc87df9188474215d15d2bb6f233144bd
Erhan Ince <erhanince@Erhans-MacBook-Pro.local>
2020-07-08 14:20:28 +0200

    Counter

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+let ergeb = document.getElementById('ergeb');
+function plusOne() {
+     ergeb.innerHTML = Number(ergeb.innerHTML) + 1;
+}
+function minusOne() {
+     ergeb.innerHTML = Number(ergeb.innerHTML) - 1;
+}
+function plusTen() {
+     ergeb.innerHTML = Number(ergeb.innerHTML) + 10;
+}
+function minusTen() {
+     ergeb.innerHTML = Number(ergeb.innerHTML) - 10;
+}
+function plusHundert() {
+     ergeb.innerHTML = Number(ergeb.innerHTML) + 100;
+}
+function minusHundert() {
+     ergeb.innerHTML = Number(ergeb.innerHTML) - 100;
+}
+function reset() {
+     ergeb.innerHTML = 0;
+}
+function double() {
+     ergeb.innerHTML = Number(ergeb.innerHTML) * 2;
+}
